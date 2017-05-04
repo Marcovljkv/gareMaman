bb949a17b05abd1373cc7c6069a27ffe75668cce
Piktato <ardi.rmsh@eduge.ch>
2017-05-04 15:31:30 +0200

    interface finie

--- anagramme/anagramme/frmMain.Designer.cs
+++ anagramme/anagramme/frmMain.Designer.cs
@@ -29,33 +29,134 @@
         private void InitializeComponent()
         {
             this.gbxQuestion = new System.Windows.Forms.GroupBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.lblReponse = new System.Windows.Forms.Label();
+            this.Réponse = new System.Windows.Forms.GroupBox();
+            this.btnNouvellePartie = new System.Windows.Forms.Button();
+            this.btnReponse = new System.Windows.Forms.Button();
+            this.btnSuivant = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gbxQuestion
             // 
-            this.gbxQuestion.Location = new System.Drawing.Point(34, 41);
+            this.gbxQuestion.Location = new System.Drawing.Point(12, 108);
             this.gbxQuestion.Name = "gbxQuestion";
-            this.gbxQuestion.Size = new System.Drawing.Size(972, 142);
+            this.gbxQuestion.Size = new System.Drawing.Size(1023, 118);
             this.gbxQuestion.TabIndex = 0;
             this.gbxQuestion.TabStop = false;
-            this.gbxQuestion.Text = "groupBox1";
+            this.gbxQuestion.Text = "Question";
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(925, 92);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(67, 13);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "Question N°:";
+            // 
+            // lblReponse
+            // 
+            this.lblReponse.AutoSize = true;
+            this.lblReponse.Location = new System.Drawing.Point(861, 248);
+            this.lblReponse.Name = "lblReponse";
+            this.lblReponse.Size = new System.Drawing.Size(131, 13);
+            this.lblReponse.TabIndex = 3;
+            this.lblReponse.Text = "Nombre de réponse juste :";
+            // 
+            // Réponse
+            // 
+            this.Réponse.Location = new System.Drawing.Point(12, 264);
+            this.Réponse.Name = "Réponse";
+            this.Réponse.Size = new System.Drawing.Size(1023, 118);
+            this.Réponse.TabIndex = 2;
+            this.Réponse.TabStop = false;
+            this.Réponse.Text = "Question";
+            // 
+            // btnNouvellePartie
+            // 
+            this.btnNouvellePartie.Location = new System.Drawing.Point(12, 403);
+            this.btnNouvellePartie.Name = "btnNouvellePartie";
+            this.btnNouvellePartie.Size = new System.Drawing.Size(186, 48);
+            this.btnNouvellePartie.TabIndex = 4;
+            this.btnNouvellePartie.Text = "Nouvelle partie";
+            this.btnNouvellePartie.UseVisualStyleBackColor = true;
+            // 
+            // btnReponse
+            // 
+            this.btnReponse.Location = new System.Drawing.Point(291, 403);
+            this.btnReponse.Name = "btnReponse";
+            this.btnReponse.Size = new System.Drawing.Size(186, 48);
+            this.btnReponse.TabIndex = 5;
+            this.btnReponse.Text = "Afficher réponse";
+            this.btnReponse.UseVisualStyleBackColor = true;
+            // 
+            // btnSuivant
+            // 
+            this.btnSuivant.Location = new System.Drawing.Point(570, 403);
+            this.btnSuivant.Name = "btnSuivant";
+            this.btnSuivant.Size = new System.Drawing.Size(186, 48);
+            this.btnSuivant.TabIndex = 6;
+            this.btnSuivant.Text = "Question suivante";
+            this.btnSuivant.UseVisualStyleBackColor = true;
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.Location = new System.Drawing.Point(849, 403);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(186, 48);
+            this.btnAnnuler.TabIndex = 7;
+            this.btnAnnuler.Text = "Annule le dernier click";
+            this.btnAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.BackColor = System.Drawing.Color.Black;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.ForeColor = System.Drawing.Color.White;
+            this.lblInfo.Location = new System.Drawing.Point(12, 31);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(1023, 35);
+            this.lblInfo.TabIndex = 8;
+            this.lblInfo.Text = "Trouvez le mot caché et cliquez dans l\'ordre sur les lettres afin de le reconstit" +
+    "uer\r\n";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1047, 463);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.btnSuivant);
+            this.Controls.Add(this.btnReponse);
+            this.Controls.Add(this.btnNouvellePartie);
+            this.Controls.Add(this.lblReponse);
+            this.Controls.Add(this.Réponse);
+            this.Controls.Add(this.lblQuestion);
             this.Controls.Add(this.gbxQuestion);
             this.Name = "frmMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.GroupBox gbxQuestion;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label lblReponse;
+        private System.Windows.Forms.GroupBox Réponse;
+        private System.Windows.Forms.Button btnNouvellePartie;
+        private System.Windows.Forms.Button btnReponse;
+        private System.Windows.Forms.Button btnSuivant;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.Label lblInfo;
     }
 }
 
