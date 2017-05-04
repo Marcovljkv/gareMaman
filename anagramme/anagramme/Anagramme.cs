389d03f7dbfe21d73eee43f054e81b70721df9bf
marcovljkv <marco.vljkv@eduge.ch>
2017-05-04 15:06:44 +0200

    Création de la classe

---
+++ anagramme/anagramme/Anagramme.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anagramme
+{
+    class Anagramme
+    {
+        List<string> _listMots;
+        string _reponse;
+        int _nombreReponseJuste;
+        string _propositionUtilisateur;
+        string _question;
+        int _questionNumero;
+
+        public List<string> ListMots
+        {
+            get { return _listMots; }
+            set { _listMots = value; }
+        }
+
+        public int NombreReponseJuste
+        {
+            get { return _nombreReponseJuste; }
+            set { _nombreReponseJuste = value; }
+        }
+
+        public string PropositionUtilisateur
+        {
+            get { return _propositionUtilisateur; }
+            set { _propositionUtilisateur = value; }
+        }
+
+        public string Question
+        {
+            get { return _question; }
+            set { _question = value; }
+        }
+
+        public int QuestionNumero
+        {
+            get { return _questionNumero; }
+            set { _questionNumero = value; }
+        }
+
+        public string Reponse
+        {
+            get { return _reponse; }
+            set { _reponse = value; }
+        }
+
+        public Anagramme()
+        {
+
+
+        }
+
+        public Anagramme(string unMot)
+        {
+
+        }
+
+        void ChareListeMots(){
+
+        }
+
+        void ChoixDunMot()
+        {
+
+        }
+
+        bool isReponseOK()
+        {
+            return true;
+        }
+
+        string MelangeMot(string unMot)
+        {
+            return "";
+        }
+    }
+}
