5b5075bed9ed34111e22395bf98154b1175f2277
unknown <ardi.rmsh@eduge.ch>
2017-05-04 15:15:24 +0200

    interface

--- anagramme/anagramme/frmMain.Designer.cs
+++ anagramme/anagramme/frmMain.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gbxQuestion = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // gbxQuestion
             // 
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.gbxQuestion.Location = new System.Drawing.Point(34, 41);
+            this.gbxQuestion.Name = "gbxQuestion";
+            this.gbxQuestion.Size = new System.Drawing.Size(972, 142);
+            this.gbxQuestion.TabIndex = 0;
+            this.gbxQuestion.TabStop = false;
+            this.gbxQuestion.Text = "groupBox1";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1047, 463);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbxQuestion);
             this.Name = "frmMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.frmMain_Load);
@@ -55,7 +55,7 @@
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox gbxQuestion;
     }
 }
 
